c9f513d839388a79f5464c0bbe4d1cac8c9caeb3
DanMillerM2 <51882336+DanMillerM2@users.noreply.github.com>
2019-06-30 14:01:38 -0500

    Add files via upload

---
+++ WetlandTools/WIP_BuildRandomForest.R
@@ -0,0 +1,129 @@
+tool_exec<- function(in_params, out_params){
+  
+  #####################################################################################################  
+  ### Check/Load Required Packages  
+  #####################################################################################################   
+  arc.progress_label("Loading packages...")
+  
+  if(!requireNamespace("raster", quietly = TRUE))
+    install.packages("raster", quiet = TRUE)
+  if(!requireNamespace("sp", quitly = TRUE))
+    install.packages("sp", quite = TRUE)
+  if(!requireNamespace("rgdal", quietly = TRUE))
+    install.packages("rgdal", quiet = TRUE)
+  if(!requireNamespace("randomForest", quietly = TRUE))
+    install.packages("randomforest", quiet = TRUE)
+  
+  require(raster)
+  require(sp)
+  require(rgdal)
+  require(randomForest)
+  
+  ##################################################################################################### 
+  ### Define input/output parameters
+  #####################################################################################################
+  workingDir <- in_params[[1]]   # working directory where model files will be stored
+  inputRasters <- in_params[[2]] # list of input rasters
+  inputPoints <- in_params[[3]]  # point feature class of points classified by wetland type
+  fieldName <- in_params[[4]]    # field within the feature class with the classification
+  isWet <- in_params[[5]]        # class for is-a-wetland
+  notWet <- in_params[[6]]       # class for not-a-wetland, thes could be expanded to allow multiple names
+  modelName <- in_params[[7]]    # file name for model stored to disk in working directory. 
+  #                                Model consists of two files, modelName.RFmodel and modelName.rasterList
+  outputProbRaster <- out_params[[1]]
+ 
+  setwd(workingDir)
+  cat(paste0("Current working directory: ", workingDir,"\n"))
+  
+  ##################################################################################################### 
+  ### Load data and create dataframe object to feed to randomForest function 
+  #####################################################################################################
+  arc.progress_label("Loading data...")
+  
+  # Load the set of input rasters to a rasterstack
+  rasters <- stack(inputRasters)
+  
+  # Open the feature class with the training dataset points as a data frame
+  allPoints <- arc.open(inputPoints)
+  
+  # Keep only the column with the input field that holds the wetland Class
+  allPoints <- arc.select(object = allPoints, fields=fieldName)
+  
+  # Rename the column heading to Class
+  names(allPoints)[1] <- "Class"  
+  
+  # Translate to a spatial dataset
+  points <- arc.data2sp(allPoints)
+  
+  # Find the raster values at the point locations
+  pointValues <- raster::extract(rasters, points, method='bilinear')
+
+  # Append the class values as the first column
+  pointValues <- cbind(points[,1],pointValues)
+  
+  # Convert to a data frame
+  pointValues <- as.data.frame(pointValues)
+  
+  # Keep only records with one of the requested input field (class) values
+  pointValues <- pointValues[pointValues$Class == isWet[1]|pointValues$Class == notWet[1],]
+  
+  # Eliminate rows with NA values
+  pointValues <- na.omit(pointValues)
+  
+  # Eliminate columns with coordinate values
+  coords <- names(pointValues) %in% c("coords.x1","coords.x2")
+  training <- pointValues[!coords]
+  
+  # Save the name of the rasters used to build this model
+  rasterNames <- names(training)[-1]
+  filename <- paste0(modelName[1],".RasterList")
+  save(rasterNames,file=filename)
+  cat(paste0("Raster names saved in ",filename,"\n"))
+  
+  # Change to generic column headings; the same headings will be used for using this RF model on other basins
+  for (i in 2:length(training)) {
+    cat(paste0("Raster",i-1,": ",names(training)[i],"\n"))
+    names(training)[i] <- paste0("Raster",i-1)
+  }
+  
+  # Print summaries of the independent variable values for wetland and not-a-wetland points
+  #className <- in_params[[4]][1]
+  #print(paste0("Class ", className))
+  #print(summary(training[training$Class == in_params[[4]][1],]))
+  #className <- in_params[[5]][1]
+  #print(paste0("Class ", className))
+  #print(summary(training[training$Class == in_params[[5]][1],]))
+  
+  #####################################################################################################
+  ### Build Random Forest Model
+  #####################################################################################################
+  # Run the randomForest. 1st iteration.
+  arc.progress_label("Building random forest...")  
+  rfclass <- randomForest(training[,-1], as.factor(training$Class), ntree=200, importance=TRUE)
+
+  #####################################################################################################
+  ### Write Output
+  ##################################################################################################### 
+  # Consider additional output to a .RFlog file that gives details of the model
+  modelName <- paste0(modelName[1],".RFmodel")
+  save(rfclass,file=modelName)
+  print(rfclass)
+  #print(summary(rfclass)) # not sure most users would find this very useful
+  print(importance(rfclass))
+  varImpPlot(rfclass,sort=TRUE)
+  cat(paste0("..Model saved as ", modelName,"\n"))    
+#  plot(rfclass) # I can't get two plots to both show up
+
+# If an output probability raster is specified, create it 
+  if (!is.null(outputProbRaster) && outputProbRaster != "NA") {
+    arc.progress_label("Creating probability raster...")
+    
+  # Rename layer names in the rasterstack to match the column names in the data frame used to train the model 
+    for (i in 1:nlayers(rasters)) {names(rasters)[i] <- paste0("Raster",i)}
+    
+    predict(rasters, rfclass, type = "prob", filename = outputProbRaster, format = "GTiff")
+    print(paste0("Created GeoTiff probability raster ",outputProbRaster[1]))
+  }
+  
+  return(out_params)
+ }  
