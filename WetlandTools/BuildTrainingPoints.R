dca1c0370ae1c691af432cf9d400a3143df02746
daniellrgn <tatebrasel@gmail.com>
2021-08-26 13:42:29 -0700

    let BuildTrainingPoints users define a region margin

--- WetlandTools/BuildTrainingPoints.R
+++ WetlandTools/BuildTrainingPoints.R
@@ -5,9 +5,9 @@ tool_exec <- function(in_params, out_params) {
   workingDir <- in_params[[1]]           # Working directory where files will be read from and written to
   regionPolyFile <- in_params[[2]]       # Name of region polygon file
   wetlandPolysFile <- in_params[[3]]     # Name of wetland polygons file
-  invalidWetlandTypes <- in_params[[4]]  # Names of invalid wetland types
-  wetlandSampleRate <- in_params[[5]]    # Samples per km^2 of wetland
-  nonwetlandSampleRate <- in_params[[6]] # Samples per km^2 of non-wetland
+  wetlandSampleRate <- in_params[[4]]    # Samples per km^2 of wetland
+  nonwetlandSampleRate <- in_params[[5]] # Samples per km^2 of non-wetland
+  regionMargin <- in_params[[6]]         # Width of region interior margin
   trainingPointsFile <- out_params[[1]]  # Name of output training points file
   
   # Helper functions -----------------------------------------------------------
@@ -34,25 +34,42 @@ tool_exec <- function(in_params, out_params) {
   
   # Validate parameters --------------------------------------------------------
   
-  # Sample wetlands ------------------------------------------------------------
+  if (!file.exists(workingDir))
+    stop("Could not find working directory: '", workingDir, "'\n")
   
   setwd(workingDir)
+  
+  if (!file.exists(regionPolyFile))
+    stop("Could not find region shape file: '", regionPolyFile, "'\n")
+  
+  if (!file.exists(wetlandPolysFile))
+    stop("Could not find wetland shape file: '", wetlandPolysFile, "'\n")
+  
+  if (!(is.numeric(wetlandSampleRate) && length(wetlandSampleRate) == 1))
+    stop("Wetland sample rate must be a single numeric value")
+  
+  if (!(is.numeric(nonwetlandSampleRate) && length(nonwetlandSampleRate) == 1))
+    stop("Non-wetland sample rate must be a single numeric value")
+  
+  # Prepare the region ---------------------------------------------------------
+  
+  # Load region polygon
+  regionPoly <- terra::vect(regionPolyFile)
+  
+  terra::plot(regionPoly)
+  
+  # Shrink region by applying an interior margin. This ensures that training 
+  # points will not be sampled near the region's edges
+  regionPoly <- terra::buffer(regionPoly, width = -abs(regionMargin))
+  
+  # Sample wetlands ------------------------------------------------------------
   
   # Load wetland polygons
   wetlandPolys <- terra::vect(wetlandPolysFile)
   
-  # Remove invalid wetland polygons
-  #wetlandPolys <- wetlandPolys[!(wetlandPolys$WETLAND_TY %in% invalidWetlandTypes)]
-  
   # Crop the wetland polygons to the region
-  regionPoly <- terra::vect(regionPolyFile) # TODO: Shrink the region poly a bit so no points are
   wetlandPolys <- terra::project(wetlandPolys, regionPoly)
   wetlandPolys <- terra::crop(wetlandPolys, regionPoly)
-  
-  # TODO: Avoid sampling close to the region's edges. Generated topo rasters 
-  # sometimes have strange values at their extremities, so we shouldn't train a 
-  # model with values extracted at those locations. This could be as easy as 
-  # shrinking the region polygon, but only if the region is convex.
   
   # Sample wetland regions
   wetlandCoords <- samplePolys(wetlandPolys, wetlandSampleRate)
@@ -70,14 +87,17 @@ tool_exec <- function(in_params, out_params) {
   
   trainingCoords <- rbind(wetlandCoords, nonwetlandCoords)
   trainingAtts <- data.frame(class = factor(c(rep("WET", nrow(wetlandCoords)), rep("UPL", nrow(nonwetlandCoords)))))
-  trainingPoints <- terra::vect(trainingCoords, atts = trainingAtts)
+  trainingPoints <- terra::vect(trainingCoords, atts = trainingAtts, crs = terra::crs(regionPoly))
   
-  #terra::plot(regionPoly)
-  #terra::polys(wetlandPolys)
-  #terra::points(trainingPoints, col = c(rep("blue", nrow(wetlandCoords)), rep("red", nrow(nonwetlandCoords))))
+  terra::polys(regionPoly, lty = 2)
+  terra::polys(wetlandPolys, border = "cyan")
+  terra::points(trainingPoints, col = c(rep("blue", nrow(wetlandCoords)), rep("red", nrow(nonwetlandCoords))))
   
   # Create training points file ------------------------------------------------
   
+  if (!(endsWith(trainingPointsFile, ".shp")))
+    trainingPointsFile <- paste0(trainingPointsFile, ".shp")
+    
   terra::writeVector(trainingPoints, trainingPointsFile, overwrite = TRUE)
   
   return(out_params)
@@ -105,9 +125,9 @@ if (FALSE) {
       workingDir = "C:/Work/Data/pack_forest",
       regionPolyFile = "PF_studyarea.shp",
       wetlandPolysFile = "pf_wetlandPolys.shp",
-      invalidWetlandTypes = c(),
-      wetlandSampleRate = 50,
-      nonwetlandSampleRate = 10
+      wetlandSampleRate = 100,
+      nonwetlandSampleRate = 20,
+      regionMargin = 50
     ),
     out_params = list(
       trainingPointsFile = "pf_training.shp"
