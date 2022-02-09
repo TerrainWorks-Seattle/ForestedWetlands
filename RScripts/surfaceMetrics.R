d78d3c2a4dbfe7be29c7e62a03c564e34aa09f1b
Helen Miller <helen@ohana.org>
2022-02-09 14:31:20 -0800

    Add TWI to Rscripts

--- RScripts/surfaceMetrics.R
+++ RScripts/surfaceMetrics.R
@@ -13,18 +13,23 @@ config <- yaml::read_yaml(file.path(rscriptDir, "surfaceMetrics.config"))
 
 if (!setequal(names(config), 
               c("DEM_path", 
-                "scratch_folder", 
+                "scratch_folder",
+                "overwrite",
                 "executable_path", 
+                "output_suffix",
                 "length_scale", 
                 "metrics", 
-                "sample",
-                "output_suffix"))) {
+                "dev_options", 
+                "pca_options"))) {
   stop("Invalid config")
 }
 if (!all(names(config$metrics) %in% c("grad", 
                                       "plan", 
-                                      "prof", 
-                                      "dev"))) {
+                                      "prof",
+                                      "bcon",
+                                      "dev", 
+                                      "twi", 
+                                      "pca"))) {
   stop("Invalid surface metrics specified")
 }
 
@@ -34,6 +39,8 @@ if (!dir.exists(config$executable_path)) {
 executable_path <- normalizePath(config$executable_path)
 
 # TODO: Check for executable files in executable_path
+# TODO: add "overwrite" parameter, and if FALSE, don't re-calculate 
+# existing grids
 # TODO: Convert DEM file type if needed
 # TODO: Convert length
 # TODO: Do smart things with downsampling
@@ -71,25 +78,100 @@ setwd(wd)
 
 # --- localRelief (DEV) ---
 
-localRelief_inputFile_path <- 
-  normalizePath(file.path(config$scratch_folder, "input_localRelief.txt"))
+if (!is.null(config$metrics$dev)) {
+  localRelief_inputFile_path <- 
+    normalizePath(file.path(config$scratch_folder, "input_localRelief.txt"))
+  
+  write_input_file_localRelief(
+    DEM_path = config$DEM_path, 
+    length_scale = config$length_scale, 
+    scratch_folder = config$scratch_folder, 
+    resample = config$dev_options$resample, 
+    interval = config$dev_options$sampleInterval, 
+    filename = localRelief_inputFile_path, 
+    output_file_extension = config$output_suffix
+  )
+  
+  localRelief <- paste0(executable_path, "\\LocalRelief.exe")
+  command <- paste(localRelief, localRelief_inputFile_path, sep = " ")
+  setwd(config$scratch_folder)
+  output <- system(command, 
+                   wait = TRUE)
+  setwd(wd)
+}
 
-write_input_file_localRelief(
-  DEM_path = config$DEM_path, 
-  length_scale = config$length_scale, 
-  scratch_folder = config$scratch_folder, 
-  resample = config$sample$downSample, 
-  interval = config$sample$sampleInterval, 
-  filename = localRelief_inputFile_path, 
-  output_file_extension = config$output_suffix
-)
+# --- topographic wetness index ---
 
-localRelief <- paste0(executable_path, "\\LocalRelief.exe")
-command <- paste(localRelief, localRelief_inputFile_path, sep = " ")
-setwd(config$scratch_folder)
-output <- system(command, 
-                 wait = TRUE)
-setwd(wd)
+if (!is.null(config$metrics$twi)) {
+  # First make sure all relevant input files are present
+  # 
+  missing_metrics <- c()
+  grad_path <- file.path(config$scratch_folder, 
+                         paste0("grad", config$output_suffix, ".flt"))
+  plan_path <- file.path(config$scratch_folder, 
+                         paste0("plan", config$output_suffix, ".flt"))
+  bcon_path <- file.path(config$scratch_folder, 
+                         paste0("bcon", config$output_suffix, ".flt"))
+  if (!file.exists(grad_path)) {
+    missing_metrics <- c(missing_metrics, "grad")
+  }
+  if (!file.exists(plan_path)) {
+    missing_metrics <- c(missing_metrics, "plan")
+  }
+  if (!file.exists(bcon_path)) {
+    missing_metrics <- c(missing_metrics, "bcon")
+  }
+  
+  if (length(missing_metrics) > 0) {
+    # Run MakeGrids for all missing metrics
+    twi_makeGrids_inputFile_path <- 
+      normalizePath(file.path(config$scratch_folder, "twi_input_makeGrids.txt"))
+    
+    write_input_file_MakeGrids(
+      DEM_path = config$DEM_path, 
+      length_scale = config$length_scale, 
+      scratch_folder = config$scratch_folder, 
+      grad = "grad" %in% missing_metrics, 
+      plan = "plan" %in% missing_metrics,  
+      prof = FALSE,
+      bcon = "bcon" %in% missing_metrics, 
+      filename = twi_makeGrids_inputFile_path,
+      output_file_extension = config$output_suffix
+    )
+    
+    makeGrids <- paste0(executable_path, "\\MakeGrids.exe")
+    command <- paste(makeGrids, twi_makeGrids_inputFile_path, sep = " ")
+    # Need wd to be scratch dir because that is where files are written
+    setwd(config$scratch_folder)
+    output <- system(command, 
+                     wait = TRUE)
+    setwd(wd)
+  }
+  
+  buildGrids_inputFile_path <- 
+    normalizePath(file.path(config$scratch_folder, "input_buildGrids.txt"))
+  
+  write_input_file_buildGrids(
+    DEM_path = config$DEM_path, 
+    length_scale = config$length_scale, 
+    scratch_folder = config$scratch_folder, 
+    grad_path = grad_path, 
+    plan_path = plan_path, 
+    bcon_path = bcon_path, 
+    filename = buildGrids_inputFile_path, 
+    output_file_extension = config$output_suffix
+  )
+  
+  
+  buildGrids <- paste0(executable_path, "\\BuildGrids.exe")
+  command <- paste(buildGrids, buildGrids_inputFile_path, sep = " ")
+  setwd(config$scratch_folder)
+  output <- system(command, 
+                   wait = TRUE)
+  setwd(wd)
+}
+
+# --- 
 
 # TODO: Reformat .flt files as .tif
 # TODO: Add topographic wetness index (BuildGrids.exe)
